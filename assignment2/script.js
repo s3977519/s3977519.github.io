dd07052a210315d364e0e8b83b07d544f46cdeb8
renalyn11 <s3977519@student.rmit.edu.au>
2024-05-19 00:22:34 +1000

    designed

---
+++ assignment2/script.js
@@ -0,0 +1,44 @@
+const video = document.querySelector("#custom-video-player");
+const playPauseBtn = document.querySelector("#play-pause-btn");
+const playPauseImg = document.querySelector("#play-pause-img");
+const progressBar = document.querySelector("#progress-bar-fill");
+video.removeAttribute("controls");
+// playPauseBtn.addEventListener("click", togglePlayPause);
+video.addEventListener("timeupdate", updateProgressBar);
+function togglePlayPause() {
+  if (video.paused || video.ended) {
+    video.play();
+    playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/pause--v1.png";
+  } else {
+    video.pause();
+    playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/play--v1.png";
+  }
+}
+function updateProgressBar() {
+  const value = (video.currentTime / video.duration) * 100;
+  progressBar.style.width = value + "%";
+}
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+
+    var icon = this.children[0];
+    if (icon.src === "https://img.icons8.com/ios-glyphs/30/plus.png") {
+      icon.src = "https://img.icons8.com/ios-glyphs/30/minus.png";
+    } else {
+      icon.src = "https://img.icons8.com/ios-glyphs/30/plus.png";
+    }
+
+    
+  });
+}
