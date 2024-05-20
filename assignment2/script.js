b6fad0d5ec3e2abb8b16a4f0764a22c6151d57ff
renalyn11 <s3977519@student.rmit.edu.au>
2024-05-20 15:15:38 +1000

    update 1

--- assignment2/script.js
+++ assignment2/script.js
@@ -2,9 +2,15 @@ const video = document.querySelector("#custom-video-player");
 const playPauseBtn = document.querySelector("#play-pause-btn");
 const playPauseImg = document.querySelector("#play-pause-img");
 const progressBar = document.querySelector("#progress-bar-fill");
+
+//View Album Button Definition
+var accordion = document.getElementsByClassName("accordian"); var i;
+
+
 video.removeAttribute("controls");
+
 // playPauseBtn.addEventListener("click", togglePlayPause);
-video.addEventListener("timeupdate", updateProgressBar);
+
 function togglePlayPause() {
   if (video.paused || video.ended) {
     video.play();
@@ -14,31 +20,76 @@ function togglePlayPause() {
     playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/play--v1.png";
   }
 }
-function updateProgressBar() {
-  const value = (video.currentTime / video.duration) * 100;
-  progressBar.style.width = value + "%";
+
+// Progressbar
+
+// video.addEventListener("timeupdate", updateProgressBar);
+// function updateProgressBar() {
+//   const value = (video.currentTime / video.duration) * 100;
+//   progressBar.style.width = value + "%";
+// }
+  
+function update() { 
+  var element = document.getElementById("progressBar");    
+  var width = 1; 
+  var identity = setInterval(scene, 10); 
+  function scene() { 
+    if (width >= 100) { 
+      clearInterval(identity); 
+    } else { 
+      width++;  
+      element.style.width = width + '%';  
+      element.innerHTML = width * 1  + '%'; 
+    } 
+  } 
+} 
+
+// Repeat Button
+
+let vid = document.getElementById("custom-video-player");
+
+function enableLoop() { 
+  vid.loop = true;
+  vid.load();
+} 
+
+// Status Button Code > used to check if loop function was working, it is :D 
+
+// function checkLoop() { 
+//   alert(vid.loop);
+// } 
+
+// Fullscreen Button
+
+var elem = document.getElementById("custom-video-player");
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  }
+// not sure if needed, found these codes while doing some research on creating the fullscreen button:
+    else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
 }
 
-var acc = document.getElementsByClassName("accordion");
-var i;
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
+// Accordian View Album Button
+
+for (i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener("click", function() {
+    /* Allows for toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
     this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
     var panel = this.nextElementSibling;
     if (panel.style.display === "block") {
       panel.style.display = "none";
     } else {
       panel.style.display = "block";
     }
-
-    var icon = this.children[0];
-    if (icon.src === "https://img.icons8.com/ios-glyphs/30/plus.png") {
-      icon.src = "https://img.icons8.com/ios-glyphs/30/minus.png";
-    } else {
-      icon.src = "https://img.icons8.com/ios-glyphs/30/plus.png";
-    }
-
-    
   });
 }
+
