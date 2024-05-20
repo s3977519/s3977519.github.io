2eb734b59f9e4ed5d1c93892174256fd89558094
renalyn11 <s3977519@student.rmit.edu.au>
2024-05-20 15:54:22 +1000

    nah

--- assignment2/script.js
+++ assignment2/script.js
@@ -23,27 +23,12 @@ function togglePlayPause() {
 
 // Progressbar
 
-// video.addEventListener("timeupdate", updateProgressBar);
-// function updateProgressBar() {
-//   const value = (video.currentTime / video.duration) * 100;
-//   progressBar.style.width = value + "%";
-// }
+video.addEventListener("timeupdate", updateProgressBar);
+function updateProgressBar() {
+  const value = (video.currentTime / video.duration) * 100;
+  progressBar.style.width = value + "%";
+}
   
-function update() { 
-  var element = document.getElementById("progressBar");    
-  var width = 1; 
-  var identity = setInterval(scene, 10); 
-  function scene() { 
-    if (width >= 100) { 
-      clearInterval(identity); 
-    } else { 
-      width++;  
-      element.style.width = width + '%';  
-      element.innerHTML = width * 1  + '%'; 
-    } 
-  } 
-} 
-
 // Repeat Button
 
 let vid = document.getElementById("custom-video-player");
